3be8efedc4bf52b6035db39437e9e31c9ca50961
Kauã Dias <88117835+DiaSKaOs@users.noreply.github.com>
2022-03-08 20:50:56 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+function temperatura() {
+    var cidade = document.getElementById("cidade").value;
+
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${cidade}&units=metric&lang=pt_br&appid=e6d0f8c8a9bbcbef9f9953cefd2d966f`
+
+
+    let info = {
+        method: 'GET',
+        MODE: 'cors',
+        cache: 'default'
+    }
+
+    fetch(url, info)
+        .then((response) => response.json()) //Se dar certo = THEN - Exigencia json
+        .then(data => preencher(data))
+        .then((data) => console.log(data))
+
+        .catch(erro => alert("Cidade não encontrada")) //Se dar errado = catch
+
+    const preencher = (tempo) => {
+        for (const campo in tempo) {
+            document.getElementById("graus_celsius").innerText = Math.floor(tempo.main.temp) + "°C"
+            document.getElementById("info").innerText = tempo.weather[0].description.toUpperCase()
+            console.log(tempo.weather[0].description)
+        }
+    }
+    document.getElementById("nome").innerText = cidade
+}
+
